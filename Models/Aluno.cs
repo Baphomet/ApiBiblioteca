0d0719fbf736dfcbdce9b08c1a98e05eb28bf2dc
Baphomet <fgrochovski@gmail.com>
2026-03-03 17:05:27 -0300

    feat: finalizar model Aluno

--- Models/Aluno.cs
+++ Models/Aluno.cs
@@ -1,6 +1,15 @@
-﻿namespace ApiBiblioteca.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ApiBiblioteca.Models
 {
     public class Aluno
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        [JsonIgnore]
+        public ICollection<Emprestimo> Emprestimos { get; set; }
+
     }
 }
