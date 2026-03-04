3ecac171255f794f2157933494baa7b1c0739ed1
Baphomet <fgrochovski@gmail.com>
2026-03-04 09:46:25 -0300

    feat: implementar propriedades da classe EmprestimoDTO

--- DTOs/EmprestimoDTO.cs
+++ DTOs/EmprestimoDTO.cs
@@ -2,5 +2,9 @@
 {
     public class EmprestimoDTO
     {
+        public Guid LivroId { get; set; }
+        public Guid AlunoId { get; set; }
+        public DateTime DataEmprestimo { get; set; }
+        public DateTime? DataDevolucao { get; set; }
     }
 }
