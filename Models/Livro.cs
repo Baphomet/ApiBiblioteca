e4aef5905d6d68cb6661be0da697da3e377906c9
Baphomet <fgrochovski@gmail.com>
2026-03-04 09:10:43 -0300

    feat: adicionar model Livro

--- Models/Livro.cs
+++ Models/Livro.cs
@@ -2,5 +2,12 @@
 {
     public class Livro
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AnoPublicacao { get; set; }
+        public bool IsDisponivel { get; set; }
+        public ICollection<Emprestimo> Emprestimos { get; set; }
+
     }
 }
