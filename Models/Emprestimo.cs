e408840f28aeb4964c028894be25938b02bacb09
Baphomet <fgrochovski@gmail.com>
2026-03-04 09:22:52 -0300

    feat: adicionar model Emprestimo

--- Models/Emprestimo.cs
+++ Models/Emprestimo.cs
@@ -2,5 +2,12 @@
 {
     public class Emprestimo
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid LivroId { get; set; }
+        public Livro Livro { get; set; }
+        public Guid AlunoId { get; set; }
+        public Aluno Aluno { get; set; }
+        public DateTime DataEmprestimo { get; set; }
+        public DateTime? DataDevolucao { get; set; }
     }
 }
