7e3268037211db6519fea81d9fb96ca3b8fc9bf2
Baphomet <fgrochovski@gmail.com>
2026-03-04 09:43:30 -0300

    feat: implementar propriedades na classe LivroDTO

--- DTOs/LivroDTO.cs
+++ DTOs/LivroDTO.cs
@@ -2,5 +2,10 @@
 {
     public class LivroDTO
     {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AnoPublicacao { get; set; }
+        public bool IsDisponivel { get; set; }
+
     }
 }
